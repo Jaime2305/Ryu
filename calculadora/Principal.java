d0a7df88b8261bf9fb96dc9c4168b78a0f28d518
Ryu <jimmymolina2305@gmail.com>
2024-09-06 21:29:54 -0600

    listo

--- calculadora/Principal.java
+++ calculadora/Principal.java
@@ -1,48 +1,23 @@
-import Base.Opera;
-import java.util.Scanner;
-
+import Base.Menu;
 public class Principal {
     public static void main(String[] args) {
-        Opera mostrar=new Opera();
-        Scanner input=new Scanner(System.in);
-        String caso;
+        Menu mostrar=new Menu();
+        String n;
         
+        mostrar.menu();
+        n=mostrar.getCaso();
 
-      do {
-        System.out.println("Escriba que operacion desea realizar:");
-        System.out.println("1- Suma");
-        System.out.println("2- Resta");
-        System.out.println("3- Multiplicacion");
-        System.out.println("4- Division");
-        System.out.println("4- Exponencial");
-        System.out.println("5- Cerrar");
-
-        caso=input.nextLine();
-        
-        switch (caso) {
-            case "Suma":
-            mostrar.Suma();
+        do{
+        switch (n) {
+            case "6":
+           System.out.println("Saliendo...");
+           n="cerrar";
             break;
-
-            case "Resta":
-            mostrar.Resta();
-            break;
-
-            case "Multiplicacion":
-            mostrar.Multiplicacion();
-            break;
-
-            case "Cerrar":
-            System.out.println("##Programa finalizado##");
-                break;
 
             default:
-            System.out.println("##Opcion no disponible o error en digitacion###");
-                
-        
-
-        }}while (!caso.equalsIgnoreCase("Cerrar"));
-        input.close();
+            break;
+        }}while (!n.equals("cerrar"));
+      
     
         
       
